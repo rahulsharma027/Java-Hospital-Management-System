3ef17d820dec6981544b9063a6182a580ec63c3b
Rahul Sharma <88182781+GreasyGorilla@users.noreply.github.com>
2022-12-27 10:38:55 +0530

    Add files via upload

---
+++ Hospital_System_2/hospital_2.sql
@@ -0,0 +1,145 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hospital_2
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `appointment`
+--
+
+DROP TABLE IF EXISTS `appointment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `appointment` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `fullname` varchar(45) NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  `age` varchar(45) NOT NULL,
+  `appoint_date` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `phno` varchar(45) NOT NULL,
+  `diseases` varchar(500) NOT NULL,
+  `doctor_id` int NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `status` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `uid_idx` (`user_id`),
+  KEY `did_idx` (`doctor_id`),
+  CONSTRAINT `did` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`id`),
+  CONSTRAINT `uid` FOREIGN KEY (`user_id`) REFERENCES `user_dtls` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointment`
+--
+
+LOCK TABLES `appointment` WRITE;
+/*!40000 ALTER TABLE `appointment` DISABLE KEYS */;
+INSERT INTO `appointment` VALUES (1,1,'Rahul Sharma','male','21','2001-05-12','rahul@gmail.com','4444444444','Pain in Heart ',1,'Sirsi, Jaipur','Medical Attention Needed'),(2,2,'Nipun','male','19','2003-02-24','nipun@gmail.com','7777777777','Pet Dard',2,'Chitrakoot,Jaipur','Pending');
+/*!40000 ALTER TABLE `appointment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `doctor`
+--
+
+DROP TABLE IF EXISTS `doctor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doctor` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(45) NOT NULL,
+  `dob` varchar(45) NOT NULL,
+  `qualification` varchar(45) NOT NULL,
+  `specialist` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `mobno` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doctor`
+--
+
+LOCK TABLES `doctor` WRITE;
+/*!40000 ALTER TABLE `doctor` DISABLE KEYS */;
+INSERT INTO `doctor` VALUES (1,'Nakul','2000-06-11','MBBS','Heart','nakul@gmail.com','5555555555','5555'),(2,'Abhijeet','2001-06-22','B.TECH','Anatomy','abhijeet@gmail.com','66666666','6666'),(3,'Yash','2002-05-30','B.Arch','Foot','yash@gmail.com','8888888888','8888');
+/*!40000 ALTER TABLE `doctor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `specialist`
+--
+
+DROP TABLE IF EXISTS `specialist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `specialist` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `spec_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `specialist`
+--
+
+LOCK TABLES `specialist` WRITE;
+/*!40000 ALTER TABLE `specialist` DISABLE KEYS */;
+INSERT INTO `specialist` VALUES (1,'Dentist'),(2,'Heart'),(3,'Brain'),(4,'Foot'),(5,'Headache'),(6,'Anatomy');
+/*!40000 ALTER TABLE `specialist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_dtls`
+--
+
+DROP TABLE IF EXISTS `user_dtls`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_dtls` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_dtls`
+--
+
+LOCK TABLES `user_dtls` WRITE;
+/*!40000 ALTER TABLE `user_dtls` DISABLE KEYS */;
+INSERT INTO `user_dtls` VALUES (1,'Rahul Sharma','rahul@gmail.com','4444'),(2,'Nipun','nipun@gmail.com','7777');
+/*!40000 ALTER TABLE `user_dtls` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-19 16:51:08
