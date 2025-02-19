d8d590d7b7929d0a99a30379c229fac50298d06f
Rahul Sharma <88182781+rahulsharma027@users.noreply.github.com>
2025-02-19 12:45:16 +0530

    Update hospital_2.sql

--- Hospital_System_2/hospital_2.sql
+++ Hospital_System_2/hospital_2.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `appointment`
 --
 
-DROP TABLE IF EXISTS `appointment`
+DROP TABLE IF EXISTS `appointment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `appointment` (
